d18e0d2efca3654ad96f159f0ff63a344e73ccbd
Adam <adam.fire1@marist.edu>
2020-09-02 19:28:07 -0400

    HW 1

---
+++ hw/1/Problem1.java
@@ -0,0 +1,6 @@
+public class Problem1 {  
+    public static void main(String[] args){
+        System.out.print("The discriminant for the equation 3x^2 + 4x + 5 = 0 is 4^2 - 4 * 3 * 5 = ");
+        System.out.println(Math.pow(4,2)- (4 * 3 * 5));
+    }
+}
---
+++ hw/1/Problem2.java
@@ -0,0 +1,6 @@
+public class Problem2 {  
+    public static void main(String[] args){
+        System.out.print("The average acceleration is (10.5 - 5.6) / 0.5 = ");
+        System.out.println((10.5 - 5.6) / 0.5);
+    }
+}
