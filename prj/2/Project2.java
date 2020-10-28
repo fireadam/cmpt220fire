61796e99fb479720534cd73d2211371dc23ebf21
Adam <adam.fire1@marist.edu>
2020-10-28 03:37:40 -0400

    Project 2

---
+++ prj/2/Project2.java
@@ -0,0 +1,26 @@
+public class Project2 {
+    public static void main(String[] args){
+        java.util.Scanner input = new java.util.Scanner(System.in);
+        System.out.print("Enter an interest rate and an investment amount: ");
+        //rate and investment inputs
+        double rate = input.nextDouble();
+        double investment = input.nextDouble();
+        //Year array
+        int[] t = new int[30];
+        //Future Value array
+        double[] calc = new double[30];
+        System.out.println("Years  Future Value ");
+        //Loop for the display of the arrays side by side
+        for (int i = 0; i<30; i++){
+            t[i] = (i+1);
+            calc[i] = futureInvestmentValue(investment, rate/12, i+1);
+            System.out.println(t[i] + "   " + calc[i]);
+        }
+    }
+    //Method for our equation
+    public static double futureInvestmentValue(double investmentAmount, double monthlyInterestRate,int years){
+        double result = investmentAmount*Math.pow(1+monthlyInterestRate,years*12);
+        return result;
+    }
+
+}
